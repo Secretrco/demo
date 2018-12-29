fb492168e3a6544bb376bdb450c5321bf1dffa36
Aaron Hayes <aaron.hayes92@gmail.com>
2018-12-29 22:42:16 +1000

    Add GoodComponent to react app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import GoodComponent from './components/GoodComponent';
+
 class App extends Component {
   render() {
     return (
@@ -20,6 +22,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <GoodComponent />
       </div>
     );
   }
---
+++ react-app/src/components/GoodComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class GoodComponent extends Component {
+  render() {
+    return (
+      <div>
+        Secretr is the best! Try it now.
+      </div>
+    );
+  }
+}
+
+export default GoodComponent;
